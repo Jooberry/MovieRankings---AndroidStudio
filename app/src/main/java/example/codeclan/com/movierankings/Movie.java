6d8730d5bed202899d80250880d6526e2009ab1c
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-04-17 15:34:58 +0100

    created initial files

---
+++ app/src/main/java/example/codeclan/com/movierankings/Movie.java
@@ -0,0 +1,21 @@
+package example.codeclan.com.movierankings;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by user on 17/04/2017.
+ */
+
+public class Movie {
+    private String title;
+    private String genre;
+    private double rating;
+
+    public Robot(String title, String genre, double rating) {
+        this.title = title;
+        this.genre = genre;
+        this.rating = rating;
+    }
+
+
+}
---
+++ app/src/test/java/example/codeclan/com/movierankings/MovieTest.java
@@ -0,0 +1,8 @@
+package example.codeclan.com.movierankings;
+
+/**
+ * Created by user on 17/04/2017.
+ */
+
+public class MovieTest {
+}
